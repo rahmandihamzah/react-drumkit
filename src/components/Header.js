12a738224cace3752263145f6a51d5584060e7f3
Rahmandi Hamzah <rahmandi.dev@gmail.com>
2019-05-20 13:12:05 +0700

    styles folder and files added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,11 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 class Header extends React.Component {
     render() {
         return (
-            <div>
-                <Link to="/">Home</Link>
-                <Link to="/products">Product</Link>
+            <div className="container">
+                <div className="header">
+                    <Link to="/">Home</Link>
+                    <Link to="/products">Product</Link>
+                </div>
             </div>
         )
 
